1ecebd9bd3b4e23f0bcd678eed05aee8cf314455
Anton Karliner <antonkarliner@gmail.com>
2023-06-03 01:12:20 +0200

    Lato font for web app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart'
+    show kIsWeb, LicenseRegistry, LicenseEntryWithLineBreaks;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/coffee_timer_app.dart';
 import 'models/brewing_method.dart';
 import 'models/recipe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    LicenseRegistry.addLicense(() async* {
+      final license =
+          await rootBundle.loadString('assets/google_fonts/OFL.txt');
+      yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+    });
+  }
+
   List<BrewingMethod> brewingMethods = await loadBrewingMethodsFromAssets();
-  runApp(CoffeeTimerApp(brewingMethods: brewingMethods));
+
+  runApp(MyApp(brewingMethods: brewingMethods));
+}
+
+class MyApp extends StatelessWidget {
+  final List<BrewingMethod> brewingMethods;
+
+  MyApp({required this.brewingMethods});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Timer App',
+      theme: ThemeData(
+        textTheme: kIsWeb
+            ? GoogleFonts.latoTextTheme(
+                Theme.of(context).textTheme,
+              )
+            : Theme.of(context).textTheme,
+      ),
+      home: CoffeeTimerApp(brewingMethods: brewingMethods),
+    );
+  }
 }
 
 Future<List<BrewingMethod>> loadBrewingMethodsFromAssets() async {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import in_app_review
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 import wakelock_macos
@@ -14,6 +15,7 @@ import wakelock_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
