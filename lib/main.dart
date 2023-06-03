e763c3b26911716a8263c3f07c1dea1c40064827
Anton Karliner <antonkarliner@gmail.com>
2023-06-03 02:29:48 +0200

    new app icons for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
         fontFamily: kIsWeb ? 'Lato' : null,
       ),
       home: CoffeeTimerApp(brewingMethods: brewingMethods),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
