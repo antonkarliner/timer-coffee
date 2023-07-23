d92a37d93e1b032b4851b9be6db53c8837b7bf30
Anton Karliner <antonkarliner@gmail.com>
2023-07-23 13:00:24 +0200

    Revert "og:title for recipe detail"
    This reverts commit 6f793be206af64df88bd5a80247d369daa5660a1.

--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -55,20 +55,7 @@ class _RecipeDetailScreenState extends State<RecipeDetailScreen> {
     initialRatio = originalWater! / originalCoffee!;
     _coffeeController.text = _updatedRecipe!.coffeeAmount.toString();
     _waterController.text = _updatedRecipe!.waterAmount.toString();
-    if (kIsWeb && _updatedRecipe != null) {
-      updateOgTitle('Check out ${_updatedRecipe!.name} on Timer.Coffee');
-    }
     setState(() {});
-  }
-
-  void updateOgTitle(String newTitle) {
-    html.MetaElement? ogTitleMetaTag =
-        html.querySelector('meta[property="og:title"]') as html.MetaElement?;
-    if (ogTitleMetaTag == null) {
-      ogTitleMetaTag = html.MetaElement()..setAttribute('property', 'og:title');
-      html.document.head!.append(ogTitleMetaTag);
-    }
-    ogTitleMetaTag.content = newTitle;
   }
 
   @override
