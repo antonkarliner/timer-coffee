070623564f6f82701ea05f09bd9bf7b9f4b3a54a
Anton Karliner <antonkarliner@gmail.com>
2023-12-20 13:52:30 +0100

    snapshot fixes

--- ios/Timer.CoffeeUITests/Timer_CoffeeUITestsLaunchTests.swift
+++ ios/Timer.CoffeeUITests/Timer_CoffeeUITestsLaunchTests.swift
@@ -10,7 +10,7 @@ import XCTest
 final class Timer_CoffeeUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
+        false
     }
 
     override func setUpWithError() throws {
