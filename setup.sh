fc7306e72bcc7f00f3f95fd02c18e9de7264b5e1
Anton Karliner <antonkarliner@gmail.com>
2024-03-24 00:31:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 git clone https://github.com/flutter/flutter.git
 ls
 flutter/bin/flutter clean
+flutter/bin/flutter pub get
 flutter/bin/flutter config --enable-web
 flutter/bin/flutter channel stable
 flutter/bin/flutter upgrade
 flutter/bin/flutter doctor
 flutter/bin/flutter pub get
-flutter/bin/dart run build_runner build --delete-conflicting-outputs
+flutter/bin/flutter pub run build_runner build --delete-conflicting-outputs
 npm install cheerio
