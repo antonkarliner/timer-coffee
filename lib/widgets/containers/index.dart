6e539ab792db9965a65eba6f0a5cefa7656b12f6
Anton Karliner <antonkarliner@gmail.com>
2025-10-05 21:47:55 +0200

    removed examples

--- lib/widgets/containers/index.dart
+++ lib/widgets/containers/index.dart
@@ -1,5 +1,3 @@
 // Container components for standardized UI elements
 export 'section_card.dart';
-export 'section_card_example.dart';
 export 'sticky_action_bar.dart';
-export 'sticky_action_bar_example.dart';
