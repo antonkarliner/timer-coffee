8cd5d93ed9721752c2cda965b423298ebfd9ada3
Anton Karliner <antonkarliner@gmail.com>
2025-11-02 01:05:46 +0100

    Fix Android link issues
    Added <queries> to AndroidManifest.xml to support url_launcher on Android 11+. Refactored RichTextLinks to a StatefulWidget and properly disposes TapGestureRecognizers to fix link tap issues on Android.

--- lib/widgets/recipe_detail/rich_text_links.dart
+++ lib/widgets/recipe_detail/rich_text_links.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-/// Stateless widget that renders simple markdown-like links [text](url).
+/// Stateful widget that renders simple markdown-like links [text](url).
 /// It intentionally contains no business logic; consumers should handle
 /// link launching via the provided onTapUrl callback.
-class RichTextLinks extends StatelessWidget {
+/// Properly disposes of TapGestureRecognizer to fix Android link issues.
+class RichTextLinks extends StatefulWidget {
   final String text;
   final TextStyle? style;
   final void Function(Uri url)? onTapUrl;
@@ -17,12 +18,30 @@ class RichTextLinks extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<RichTextLinks> createState() => _RichTextLinksState();
+}
+
+class _RichTextLinksState extends State<RichTextLinks> {
+  // Keep track of TapGestureRecognizers to properly dispose of them
+  final List<TapGestureRecognizer> _recognizers = [];
+
+  @override
+  void dispose() {
+    // Clean up all recognizers when widget is disposed
+    for (final recognizer in _recognizers) {
+      recognizer.dispose();
+    }
+    _recognizers.clear();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final RegExp linkRegExp = RegExp(r'\[(.*?)\]\((.*?)\)');
-    final Iterable<RegExpMatch> matches = linkRegExp.allMatches(text);
+    final Iterable<RegExpMatch> matches = linkRegExp.allMatches(widget.text);
 
     final TextStyle defaultTextStyle =
-        style ?? Theme.of(context).textTheme.bodyLarge!;
+        widget.style ?? Theme.of(context).textTheme.bodyLarge!;
     final Color linkColor = Theme.of(context).colorScheme.secondary;
 
     final List<TextSpan> spanList = <TextSpan>[];
@@ -30,7 +49,8 @@ class RichTextLinks extends StatelessWidget {
     int lastMatchEnd = 0;
 
     for (final match in matches) {
-      final String precedingText = text.substring(lastMatchEnd, match.start);
+      final String precedingText =
+          widget.text.substring(lastMatchEnd, match.start);
       final String linkText = match.group(1)!;
       final String linkUrl = match.group(2)!;
 
@@ -38,27 +58,31 @@ class RichTextLinks extends StatelessWidget {
         spanList.add(TextSpan(text: precedingText, style: defaultTextStyle));
       }
 
+      // Create a new TapGestureRecognizer and track it for disposal
+      final recognizer = TapGestureRecognizer()
+        ..onTap = () {
+          final uri = Uri.tryParse(linkUrl);
+          if (uri != null) {
+            widget.onTapUrl?.call(uri);
+          }
+        };
+      _recognizers.add(recognizer);
+
       spanList.add(
         TextSpan(
           text: linkText,
           style: defaultTextStyle.copyWith(color: linkColor),
-          recognizer: TapGestureRecognizer()
-            ..onTap = () {
-              final uri = Uri.tryParse(linkUrl);
-              if (uri != null) {
-                onTapUrl?.call(uri);
-              }
-            },
+          recognizer: recognizer,
         ),
       );
 
       lastMatchEnd = match.end;
     }
 
-    if (lastMatchEnd < text.length) {
+    if (lastMatchEnd < widget.text.length) {
       spanList.add(
         TextSpan(
-          text: text.substring(lastMatchEnd),
+          text: widget.text.substring(lastMatchEnd),
           style: defaultTextStyle,
         ),
       );
